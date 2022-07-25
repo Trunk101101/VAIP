71b2e1f7530121ab51b062756e01cdacf447df4c
Trunk101101 <92079891+Trunk101101@users.noreply.github.com>
2022-07-25 18:29:15 +0700

    Trung-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SubPageComponent } from './pages/sub-page/sub-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SubPageComponent
   ],
   imports: [
     BrowserModule,
